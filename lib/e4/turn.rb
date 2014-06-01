bcc9113b573058f8de85599a560d71883715edda
OpenShift guest <kailuer@web.de>
2014-06-01 05:16:49 -0400

    alignment

--- lib/e4/turn.rb
+++ lib/e4/turn.rb
@@ -2,14 +2,14 @@ class E4::Turn
   attr_accessor :moves
   
   def initialize(turn)
-      self.moves = turn
+    self.moves = turn
   end
   
   def white_move
-      moves[0]
+    moves[0]
   end
   
   def black_move
-      moves[1]
+    moves[1]
   end
 end
